8fc246a76c1337372776a766866792b9331eea04
ponponok <59070017@it.kmitl.ac.th>
2018-08-15 12:08:29 +0700

    asa

---
+++ app/src/main/java/com/example/a59070017/healthy/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.a59070017.healthy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        //setContentView(R.layout.fragment_login);
+        //setContentView(R.layout.fragment_register);
+        setContentView(R.layout.fragment_bmi);
+
+    }
+}
